95122bbd63b44391045d30f5c3dbaff9db18933b
Anh Thu Be La <127480096+ankthuw@users.noreply.github.com>
2025-09-15 22:57:42 +0700

    Update config.py

--- Hybrid_Segmentor/config.py
+++ Hybrid_Segmentor/config.py
@@ -8,13 +8,13 @@ import os
 
 ### train on total dataset
 NUM_EPOCHS = 100
-# DATASET_SIZE = {'train' : 192, 'val' : 24, 'test' : 24} # masonry
-DATASET_SIZE = {'train' : 3484, 'val' : 435, 'test' : 436} # steelcrack
+DATASET_SIZE = {'train' : 192, 'val' : 24, 'test' : 24} # masonry
+# DATASET_SIZE = {'train' : 3484, 'val' : 435, 'test' : 436} # steelcrack
 # DATASET_SIZE = {'train' : 3782, 'val' : 472, 'test' : 474} # pavement
 
 # dataset = os.path.join('../', 'sample_dataset/') # or split_dataset_final
-# dataset = "/kaggle/input/masonry/MASONRY/"
-dataset = "/kaggle/input/steels/STEELS/"
+dataset = "/kaggle/input/masonry/MASONRY/"
+# dataset = "/kaggle/input/steels/STEELS/"
 # dataset = "/kaggle/input/pavement/PAVEMENT/"
 
 # Hyperparameters etc.

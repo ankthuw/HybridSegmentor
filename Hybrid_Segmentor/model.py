462594b9290f09cdc72394ec1efd23ddee979a87
HoMinhDang <minhdang202044@gmail.com>
2025-09-26 15:04:38 +0700

    update bifusion + up

--- Hybrid_Segmentor/model.py
+++ Hybrid_Segmentor/model.py
@@ -57,23 +57,6 @@ class DepthWiseConv(nn.Module):
 
         return x
         
-class DoubleConv(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super(DoubleConv, self).__init__()
-
-        hidden_dim = int((in_dim + out_dim)/2)
-        self.conv_block = nn.Sequential(
-            nn.Conv2d(in_channels=in_dim, out_channels=hidden_dim, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=hidden_dim, out_channels=out_dim, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_dim),
-            nn.ReLU(inplace=True)
-        )
-    def forward(self, x):
-        output = self.conv_block(x)
-
-        return output
 
 class OverlapPatchEmbedding(nn.Module):
     def __init__(self, kernel, stride, padding, in_dim, out_dim):
@@ -163,16 +146,7 @@ class MiT(nn.Module):
 
         return layer_outputs
     
-class conv_upsample(nn.Module):
-    def __init__(self, scale, in_dim, out_dim=32):
-        super(conv_upsample, self).__init__()
-        self.conv = DoubleConv(in_dim, out_dim)
-        self.upscale = nn.Upsample(scale_factor=scale, mode='bilinear', align_corners=True)
 
-    def forward(self, x):
-        output = self.upscale(self.conv(x))
-        return output
-        
 
 resnet_encoder = resnet50(weights=ResNet50_Weights.DEFAULT)
 # resnet_encoder = resnet50()
@@ -239,7 +213,7 @@ class BiFusion_block(nn.Module):
         self.W_x = Conv(ch_2, ch_int, 1, bn=True, relu=False)
         self.W = Conv(ch_int, ch_int, 3, bn=True, relu=True)
 
-        self.residual = Conv(ch_1+ch_2+ch_int, ch_out, 1)
+        self.residual = Residual(ch_1+ch_2+ch_int, ch_out, 1)
         self.dropout = nn.Dropout2d(drop_rate)
 
     def forward(self, g, x):
@@ -265,6 +239,116 @@ class BiFusion_block(nn.Module):
         fuse = self.residual(torch.cat([g, x, bp], 1))
         return self.dropout(fuse)
 
+class Up(nn.Module):
+    """Upscaling then double conv"""
+    def __init__(self, in_ch1, out_ch, in_ch2=0, attn=False):
+        super().__init__()
+
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = DoubleConv(in_ch1+in_ch2, out_ch)
+
+        if attn:
+            self.attn_block = Attention_block(in_ch1, in_ch2, out_ch)
+        else:
+            self.attn_block = None
+
+    def forward(self, x1, x2=None):
+
+        x1 = self.up(x1)
+        # input is CHW
+        if x2 is not None:
+            diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
+            diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
+
+            x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                            diffY // 2, diffY - diffY // 2])
+
+            if self.attn_block is not None:
+                x2 = self.attn_block(x1, x2)
+            x1 = torch.cat([x2, x1], dim=1)
+        x = x1
+        return self.conv(x)
+
+class Attention_block(nn.Module):
+    def __init__(self,F_g,F_l,F_int):
+        super(Attention_block,self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+            )
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self,g,x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1+x1)
+        psi = self.psi(psi)
+        return x*psi
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.identity = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0),
+                nn.BatchNorm2d(out_channels)
+                )
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return self.relu(self.double_conv(x)+self.identity(x))
+
+
+class Residual(nn.Module):
+    def __init__(self, inp_dim, out_dim):
+        super(Residual, self).__init__()
+        self.relu = nn.ReLU(inplace=True)
+        self.bn1 = nn.BatchNorm2d(inp_dim)
+        self.conv1 = Conv(inp_dim, int(out_dim/2), 1, relu=False)
+        self.bn2 = nn.BatchNorm2d(int(out_dim/2))
+        self.conv2 = Conv(int(out_dim/2), int(out_dim/2), 3, relu=False)
+        self.bn3 = nn.BatchNorm2d(int(out_dim/2))
+        self.conv3 = Conv(int(out_dim/2), out_dim, 1, relu=False)
+        self.skip_layer = Conv(inp_dim, out_dim, 1, relu=False)
+        if inp_dim == out_dim:
+            self.need_skip = False
+        else:
+            self.need_skip = True
+        
+    def forward(self, x):
+        if self.need_skip:
+            residual = self.skip_layer(x)
+        else:
+            residual = x
+        out = self.bn1(x)
+        out = self.relu(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out += residual
+        return out 
+    
+
 class HybridSegmentor(pl.LightningModule):
     def __init__(self, channels=3, dims=(64, 256, 512, 1024), n_heads=(1, 2, 8, 8), 
                  expansion=(8, 8, 4, 4), reduction_ratio=(8, 4, 2, 1), n_layers=(2, 2, 2, 2), 
@@ -281,23 +365,18 @@ class HybridSegmentor(pl.LightningModule):
         self.fusion3 = BiFusion_block(ch_1=512, ch_2=512, r_2=4, ch_int=512, ch_out=512)    
         self.fusion4 = BiFusion_block(ch_1=1024, ch_2=1024, r_2=4, ch_int=1024, ch_out=1024)
 
-        # Reduce channels after fusion - input channels should match fusion output
-        self.reduce_channels_1 = DoubleConv(64, 32)      
-        self.reduce_channels_2 = DoubleConv(256, 64)     
-        self.reduce_channels_3 = DoubleConv(512, 128)   
-        self.reduce_channels_4 = DoubleConv(1024, 256)   
-        self.reduce_channels_5 = DoubleConv(2048, 256)   
+        self.up1 = Up(in_ch1=64, out_ch=32, attn=True)
+        self.up2 = Up(in_ch1=256, out_ch=64, attn=True) 
+        self.up3 = Up(in_ch1=512, out_ch=128, attn=True)
+        self.up4 = Up(in_ch1=1024, out_ch=256, attn=True)
+        self.up5 = Up(in_ch1=2048, out_ch=256, attn=True)
 
-        # Upsampling with reduced channels
-        self.upsampling_1 = conv_upsample(2, 32, 1)    
-        self.upsampling_2 = conv_upsample(4, 64, 1)    
-        self.upsampling_3 = conv_upsample(8, 128, 1)   
-        self.upsampling_4 = conv_upsample(16, 256, 1)  
-        self.upsampling_5 = conv_upsample(32, 256, 1)  
-
-        # Final 1x1 conv to combine all features
-        self.to_segment_conv = nn.Conv2d(5, 1, 1)  # Combine 5 upsampled features
-        
+        total_channels = 32 + 64 + 128 + 256 + 256  # Sum of channels from up1-up5
+        self.final = nn.Sequential(
+            Conv(total_channels, 64, 3, bn=True, relu=True),
+            Conv(64, 32, 3, bn=True, relu=True),
+            Conv(32, 1, 1, bn=False, relu=False)
+        )
         # loss function
         self.loss_fn = DiceBCELoss()
         self.loss_fn.set_debug_mode(False)  # Tắt debug info trong quá trình training
@@ -322,22 +401,45 @@ class HybridSegmentor(pl.LightningModule):
         mit_1, mit_2, mit_3, mit_4 = self.mix_transformer(x)
         output1, output2, output3, output4, side_output5 = self.cnn_encoder(x)
 
-        side_output1 = self.fusion1(output1, mit_1)
-        side_output2 = self.fusion2(output2, mit_2)
-        side_output3 = self.fusion3(output3, mit_3)
-        side_output4 = self.fusion4(output4, mit_4)
+        fused1 = self.fusion1(output1, mit_1)
+        fused2 = self.fusion2(output2, mit_2)
+        fused3 = self.fusion3(output3, mit_3)
+        fused4 = self.fusion4(output4, mit_4)
+        
+        # Progressive fusion from deep to shallow
+        # Level 4 fusion
+        fused4 = self.fusion4(output4, mit_4)  # 1024 channels
+        up4 = self.up4(fused4, output3)  # Using Up module with attention
 
-        up_side_1 = self.upsampling_1(self.reduce_channels_1(side_output1))
-        up_side_2 = self.upsampling_2(self.reduce_channels_2(side_output2))
-        up_side_3 = self.upsampling_3(self.reduce_channels_3(side_output3))
-        up_side_4 = self.upsampling_4(self.reduce_channels_4(side_output4))
-        up_side_5 = self.upsampling_5(self.reduce_channels_5(side_output5))
+        # Level 3 fusion
+        fused3 = self.fusion3(output3, mit_3)  # 512 channels
+        up3 = self.up3(fused3, output2)  # Using Up module with attention
 
-        to_fused = torch.concat((up_side_1, up_side_2, up_side_3, up_side_4, up_side_5),dim=1)
-        to_segment = self.to_segment_conv(to_fused)
+        # Level 2 fusion
+        fused2 = self.fusion2(output2, mit_2)  # 256 channels
+        up2 = self.up2(fused2, output1)  # Using Up module with attention
 
-        return to_segment, up_side_1, up_side_2, up_side_3, up_side_4, up_side_5
-    
+        # Level 1 fusion
+        fused1 = self.fusion1(output1, mit_1)  # 64 channels
+        up1 = self.up1(fused1)  # Shallowest features
+
+        # Handle deepest features
+        up5 = self.up5(side_output5)
+
+        # Ensure all features are at the same scale
+        outputs = [up1, up2, up3, up4, up5]
+        for i in range(len(outputs)):
+            if i > 0:  # up1 is already at the right scale
+                outputs[i] = F.interpolate(outputs[i], 
+                                        size=outputs[0].shape[2:],
+                                        mode='bilinear', 
+                                        align_corners=True)
+        
+        # Final prediction
+        out = self.final(torch.cat(outputs, dim=1))
+        return out, outputs[0], outputs[1], outputs[2], outputs[3], outputs[4]
+        
+        
     def training_step(self, batch, batch_idx):
         x, y = batch
         loss, pred, y = self._common_step(batch, batch_idx)
